b527d85314a2ef241a57af86b472957cf5edd75f
ferdelamad <fernando.delamadrid@checkr.com>
2019-03-21 22:57:45 -0700

    Add initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+
+  #GET request -> to the pages controller and serv the about view
+  get 'about' => 'pages#about'
+  get 'my_test' => 'contact#test'
 end
